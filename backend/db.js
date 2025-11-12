9fd60b3b657add5adcdf69a4c381f93e2f3f2fad
Richard Moraes Souza <richardmoraessouza2006@gmail.com>
2025-11-12 01:05:31 -0300

    Tirei o db Render e coloquei o Neon

--- backend/db.js
+++ backend/db.js
@@ -1,24 +1,27 @@
-import pkg from 'pg'
-import dotenv from 'dotenv'
+import pkg from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { Pool } = pkg
+const { Pool } = pkg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: false }
-})
-
-pool.connect((err, client, release) => {
-    if (err) {
-        return console.error('Erro ao conectar ao banco:', err.stack)
-    }
-    console.log('conectado ao banco!')
-    release();
+  host: process.env.PGHOST,
+  port: 5432,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: { rejectUnauthorized: false } // obrigatório no Neon
 });
 
-export default pool
+// Testa a conexão
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco:', err.stack);
+    return;
+  }
+  console.log('Conectado ao banco!');
+  release();
+});
+
+export default pool;
