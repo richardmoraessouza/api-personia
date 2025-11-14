79e5bdda998880ede2725c019fde415b996bec7f
Richard Moraes Souza <richardmoraessouza2006@gmail.com>
2025-11-14 01:23:18 -0300

    teste

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-
+console.log("teste")
 app.use("/", router);
 
 app.listen(PORT, () => {
