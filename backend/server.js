143d3ba8d71b08cea85d32218b0e2690044a86ac
Richard Moraes Souza <richardmoraessouza2006@gmail.com>
2025-11-14 01:17:58 -0300

    coloquei nova url no cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://persoia.netlify.app',
+  credentials: true
+}));
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
