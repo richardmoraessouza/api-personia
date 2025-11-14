715fefdbf6b6da2d90c750b5af6cdc78b79e2616
Richard Moraes Souza <richardmoraessouza2006@gmail.com>
2025-11-14 02:55:18 -0300

    mudei o cors de novo

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://persoia.netlify.app',
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-console.log("teste")
+
 app.use("/", router);
 
 app.listen(PORT, () => {
